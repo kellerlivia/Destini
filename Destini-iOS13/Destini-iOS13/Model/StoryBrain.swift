22de0d1506241af6236d5d8f7c7ef85b7216569a
Lívia Keller <101295392+kellerlivia@users.noreply.github.com>
2022-08-29 17:03:43 -0300

    Challenge

--- Destini-iOS13/Destini-iOS13/Model/StoryBrain.swift
+++ Destini-iOS13/Destini-iOS13/Model/StoryBrain.swift
@@ -2,7 +2,7 @@
 //  StoryBrain.swift
 //  Destini-iOS13
 //
-//  Created by Angela Yu on 08/08/2019.
+//  Created by Lívia Keller on 27/08/2022.
 //  Copyright © 2019 The App Brewery. All rights reserved.
 //
 
